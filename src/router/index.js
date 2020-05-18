352d5f18650a8ad595371886d342fe2d805b9b9a
zhangjiayu03 <zhangjiayu03@kuaishou.com>
2020-05-18 11:20:13 +0800

    更改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   base: '/',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
--- src/views/mine/Index.vue
+++ src/views/mine/Index.vue
@@ -4,15 +4,24 @@
       title="我的"
     />
 
-    <div class="avatar" @click="login">
+    <div class="avatar" v-if="isLogin">
       <img />
       <div class="name">请登录</div>
+    </div>
+
+    <div class="login-pane" v-else>
+      <van-button @click="login">请登录 / 注册</van-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    isLogin() {
+      return false
+    }
+  },
   data() {
     return {
 
@@ -31,7 +40,15 @@ export default {
   background: #f5f5f5;
 
   .avatar {
-    
+    display: flex;
+  }
+
+  .login-pane {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: cornflowerblue;
+    height: 260px;
   }
 }
 </style>
